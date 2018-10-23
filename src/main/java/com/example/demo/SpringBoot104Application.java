12261e2440fcd8f8f939439ceaddace860b64a31
zemenuyar <zemenuyar@gmail.com>
2018-10-23 13:59:15 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot104Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot104Application.class, args);
+	}
+}
